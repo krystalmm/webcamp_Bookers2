4b4a8eb6bd2d7dd2b73ae1392021c118fae4c767
krystalmm <taeyeon_aina@live.jp>
2021-02-08 13:14:25 +0000

    [update] edit page to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
+    @books = @user.books.all
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id then
+      redirect_to current_user
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/home/about' => 'homes#about' 
   
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
   
   resources :books, only: [:create, :index, :show, :destroy]
 
