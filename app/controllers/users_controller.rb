005b4dccc024ee9ac9dee4a7d4803ae25ef5e404
krystalmm <taeyeon_aina@live.jp>
2021-02-08 11:52:31 +0000

    [add] show page to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/home/about' => 'homes#about' 
   
   devise_for :users
+  resources :users, only: [:show]
   
   resources :books, only: [:create, :index, :show, :destroy]
 
