dade87a5b8a983cc28a984b3473b42ad41444b2a
krystalmm <taeyeon_aina@live.jp>
2021-02-09 05:15:54 +0000

    [add] index page to users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/home/about' => 'homes#about' 
   
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   
   resources :books, only: [:create, :index, :show, :destroy]
 
