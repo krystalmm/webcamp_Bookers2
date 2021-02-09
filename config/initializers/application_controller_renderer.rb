44bb4728ec94f78e024db5a0d871b2f6e2f96871
krystalmm <taeyeon_aina@live.jp>
2021-02-09 04:15:48 +0000

    [add] profile_image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '891a21a7343d46cc211b68cbe915df84d7d10d4c6c03f8b15380ef013e6f586ee81de4f77a32e63e14c309e3c5864a014b6f5b71eca0dbc834fcae4eb64c1daf'
